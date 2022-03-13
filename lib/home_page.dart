891639c45871495af145edd80d937bbd678ced80
smithzachary <zachary.6.smith@gmail.com>
2022-03-12 20:56:40 -0600

    widget test

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spacemulewebsite/components/_components.dart';
+import 'package:spacemulewebsite/themes/theme_provider.dart';
 
 import 'main.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:spacemulewebsite/theme.dart';
+import 'package:spacemulewebsite/themes/theme_provider.dart';
 
 import 'home_page.dart';
 
 void main() => runApp(new MyApp());
-
-class ThemeChanger with ChangeNotifier {
-  var _themeMode = ThemeMode.light;
-  ThemeMode get themeMode => _themeMode;
-  void setTheme(themeMode) {
-    _themeMode = themeMode;
-    notifyListeners();
-  }
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,18 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<ThemeChanger>(
       create: (_) => ThemeChanger(),
-      child: Builder(
-        builder: (BuildContext context) {
-          final themeChanger = Provider.of<ThemeChanger>(context);
-          return MaterialApp(
-            title: 'spaceMuleCode',
-            themeMode: themeChanger.themeMode,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            home: const HomePage(),
-          );
-        },
-      ),
+      builder: (context, child) {
+        final themeChanger = Provider.of<ThemeChanger>(context);
+        return MaterialApp(
+          title: 'spaceMuleCode',
+          themeMode: themeChanger.themeMode,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
